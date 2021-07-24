ade5ebfeb02ee18d8764c72c2e8076d20d53fe8f
Nurdien Adijaya <nurdienadijaya@gmail.com>
2021-07-24 13:55:25 +0700

    remove harshRouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
